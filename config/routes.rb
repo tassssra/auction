82063cb476194abf548250285e026243e8d67142
yasuda <tasssra@gmail.com>
2019-03-09 15:32:56 +0900

    seeds.rb, routes.rbを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/items' => 'items#index'
+  post '/items' => 'items#create'
+  get '/items/new' => 'items#new' # newとshowの順番に気をつける
+  get '/items/:id' => 'items#show' # 逆に書いてしまうと、全部showで処理されます
+  get '/items/:id/edit' => 'items#edit'
+  patch '/items/:id' => 'items#update'
+  delete '/items/:id' => 'items#destroy'
+
+# 推奨する書き方
+# Rails.application.routes.draw do
+  # resources リソース名(複数のitem(商品)を取り扱うのでitemsとなります。
+  # resources :items
+# end
+
 end

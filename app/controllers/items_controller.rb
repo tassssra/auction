bc1f1bd263356083c20878dcf9f799333da4859f
yasuda <tasssra@gmail.com>
2019-03-09 16:31:59 +0900

    index.html.erbを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,11 @@ class ItemsController < ApplicationController
   def show
     # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
     # 取得したデータを@itemというインスタンス変数にセットする
-    # @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
+
+  def index
+    @items = Item.all
+  end
+
 end

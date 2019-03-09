1f12b43fa46c622ca7056a4ddeaddb74ae5aef9c
yasuda <tasssra@gmail.com>
2019-03-09 17:34:50 +0900

    createアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,33 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+
+    # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+    @item.save
+
+    # saveの後にshow(商品詳細ページ)に飛ぶ設定をします
+    # これを設定しないと、プログラムが行き場をなくしてエラーが起きる
+    redirect_to @item
+
+    # redirect_to "/items"
+    # だとindexページにリダイレクトする
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+    # params.require(key).permit(filter)
+    # key
+    #   Strong Parameters を適用したい params の key を指定する。
+    # filter
+    #   Strong Parameters で許可するカラムを指定する。
+
+    def item_params
+      params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+    end
 
 end
